99c3769de559d5805743ff32faedd100bf706973
Aripaka Vamsy Kiran <a.vamc.it@gmail.com>
2022-07-01 12:46:51 +0530

    day5

---
+++ IncomeStatementApp02/src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = ({brand}) => (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">{brand}</a>
+        </div>
+    </nav>
+);
+
+export default Header;  
